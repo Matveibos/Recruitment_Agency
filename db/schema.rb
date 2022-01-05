d41be3b84f3720af88c8d1b3a7bff801e87e940b
matveibos <matveibos@mail.ru>
2022-01-05 19:13:37 +0300

    resolve conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 2021_12_31_133325) do
-=======
-ActiveRecord::Schema.define(version: 2021_12_24_142716) do
->>>>>>> main
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,10 +76,8 @@ ActiveRecord::Schema.define(version: 2021_12_24_142716) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-<<<<<<< HEAD
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-=======
->>>>>>> main
+
 end
