630a89c99222f69fc4886c32818b5fa44f330494
matveibos <matveibos@mail.ru>
2022-01-05 18:03:28 +0300

    create other comments

--- spec/helpers/main_helper_spec.rb
+++ spec/helpers/main_helper_spec.rb
@@ -6,10 +6,11 @@ require 'rails_helper'
 describe MainHelper do
   describe "string concat" do
     it "concats two strings with spaces" do
-      expect(helper.concat_strings("this","that")).to eq("this that")
+      expect(helper.concat_strings("this", "that")).to eq("this that")
     end
   end
 end
-RSpec.describe MainHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+# RSpec.describe MainHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
