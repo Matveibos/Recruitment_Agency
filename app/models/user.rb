8ca880779a191abb001f8f4a76f03bda63bc5c30
matveibos <matveibos@mail.ru>
2022-01-07 10:30:39 +0300

    fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  
+
   validates :firstname, presence: true
 
   has_one :tech_stack, as: :entity
-
 end
