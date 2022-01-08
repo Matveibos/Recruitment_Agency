c59e6a91b07a62e8541aae2e366aa0bda2180249
matveibos <matveibos@mail.ru>
2022-01-08 11:19:08 +0300

    fix rubocop

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
-  
+
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :technical_level, :company_name])
@@ -14,6 +14,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :technical_level, :company_name])
   end
-
-
 end
